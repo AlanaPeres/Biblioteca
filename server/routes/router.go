f6525bf598451e24d5657c1eba91d7d024eb7f07
AlanaPeres <alanalimaperes@gmail.com>
2022-09-20 18:05:09 -0300

    6

--- server/routes/router.go
+++ server/routes/router.go
@@ -1,15 +1,17 @@
 package routes
 
 import (
+	"github.com/AlanaPeres/Biblioteca/controllers"
 	"github.com/gin-gonic/gin"
 )
+
 //método
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
 	main := router.Group("api/v1")
 	{
 		books := main.Group("books")
 		{
-			books.GET("/")
+			books.GET("/", controllers.ShowAllBooks)
 		}
 	}
 	return router
