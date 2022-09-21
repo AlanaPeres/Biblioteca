2ffc2e403cf01a4113ea31a22460ceacd2251931
AlanaPeres <alanalimaperes@gmail.com>
2022-09-21 13:19:26 -0300

    11

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"github.com/hyperyuri/webapi-with-go/database"
-	"github.com/hyperyuri/webapi-with-go/server"
+	"github.com/AlanaPeres/Biblioteca/database"
+	"github.com/AlanaPeres/Biblioteca/server"
 )
 
 func main() {
 	database.StartDB()
 	s := server.NewServer()
-
 	s.Run()
 }
